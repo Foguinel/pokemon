af813d5f5030980106499961bb3b29c8da1bc036
Foguinel <foguinel@gmail.com>
2020-05-30 14:52:53 +0000

    Updating

---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+module.exports.run = async(client, message, embedColor) => {
+
+const m = await message.channel.send("Ping?");
+let embed = new Discord.RichEmbed()
+.setAuthor("Conexão 📶", message.author.avatarURL)
+.addField("A sua latência é de", `${m.createdTimestamp - message.createdTimestamp}ms`)
+.addField("A minha latência é de", `${Math.round(client.ping) -3}ms`)
+.setTimestamp()
+.setFooter(`${client.user.username}`, client.user.avatarURL)
+.setColor(embedColor)
+
+m.edit({embed});
+}
+
+module.exports.help = {
+    name: "ping",
+    description: "Comando para verificar a latência da API."
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const configBot = require("./configBot.json");
+const firebase = require("firebase")
+
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(configBot.prefix) !== 0) return;
+
+  let sender = message.author;
+  const args = message.content.slice(configBot.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  var embedColor = "0x000"
+
+    if(message.author.bot) return;
+    if(!command) return;
+    let args = message.content.split(" ").slice(1);
+    let cmd = message.content.split(" ")[0];
+    cmd = cmd.slice(configBot.prefix.length);
+    try{
+    let exec = require('./commands/' + cmd + '.js');
+    exec.run(client, message, args, firebase, sender, embedColor);
+    }catch(erro) { console.log(erro) }
+
+  })
+client.login(configBot.token)
