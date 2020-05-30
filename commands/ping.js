86ebeeca59e1c97b8ec7485cacc433c41ef5e253
Foguinel <foguinel@gmail.com>
2020-05-30 15:34:52 +0000

    updating

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async(client, message, embedColor) => {
 
+if(message.author.id !== "449940691045318656" || !message.member.hasPermission(['MANAGE_GUILDS']));
 const m = await message.channel.send("Ping?");
 let embed = new Discord.MessageEmbed()
 .setAuthor("Conexão 📶", message.author.avatarURL)
-.addField("A sua latência é de", `${m.createdTimestamp - message.createdTimestamp}ms`)
-.addField("A minha latência é de", `${Math.round(client.ping) -3}ms`)
+.addField("A latência é de", `${m.createdTimestamp - message.createdTimestamp}ms`)
 .setTimestamp()
 .setFooter(`${client.user.username}`, client.user.avatarURL)
 .setColor(embedColor)
