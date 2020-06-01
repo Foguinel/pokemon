802a4ba28a7b565fbae94682dfd41f4d83ab6c67
Foguinel <foguinel@gmail.com>
2020-06-01 02:56:29 +0000

    updating

--- commands/summon.js
+++ commands/summon.js
@@ -1,22 +1,25 @@
 const Discord = require("discord.js"); // Chama o módulo do Discord
-module.exports.run = async(client, message, embedColor) => { // Chama as outras dependências
+module.exports.run = async(client, message, embedColor) => { // Chama as outras dependências´
 
-    const fetchPokemon = () => { // Cria um void
-        const baseUrl = 'https://pokeapi.co/api/v2/pokemon/' // Define a base de URL
-        const pokemonId = Math.floor(Math.random() * 807) + 1; // Cria um valor randômico entre 1 à 807
-        const image = `https://pokeres.bastionbot.org/images/pokemon/${pokemonId}.png` // Pega uma imagem em melhor resolução
-        const url = baseUrl + `${pokemonId}` // Atualiza a URL
+    const args = message.content.slice(configBot.prefix.length).trim().split(/ +/g); // Define o que são os argumentos.
 
-        fetch(url) // Pesquisa a URL 
+    const fetchPokemon = () => { // Cria um void.
+        const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'; // Define a base de URL.
+        if(!args[0])const pokemonId = Math.floor(Math.random() * 807) + 1; // Cria um valor randômico entre 1 à 807
+        const url = baseUrl + `${pokemonId}`; // Atualiza a URL.
+
+        fetch(url) // Pesquisa a URL.
         .then(response => response.json()) // Ele transforma informações dadas pela API em um JSON
         .then(pokemon => { // Com base no JSON ele extrai informações sobre o pokemon
         
-        var name = pokemon['name'] // Compacta linhas de código.
+        var name = pokemon['name']; // Compacta linhas de código.
+        var id = pokemon['id']; // Compacta linhas de código.
+        if(!pokemonId) pokemonId = id;
+        const image = `https://pokeres.bastionbot.org/images/pokemon/${pokemonId}.png`; // Pega uma imagem em melhor resolução
 
         let embed = new Discord.MessageEmbed() // Cria um Embed.
         .setAuthor(`${name}`, image) // Define o título.
-        .addField("O id é", `${pokemon['id']}`) // Calcula o tempo necessário para editar a mensagem.
-        .setTimestamp() // Define o horário da mensagem.
+        //.setDescription("") // Calcula o tempo necessário para editar a mensagem.
         .setImage(image) // Define a imagem principal.
         .setFooter(`${client.user.username}`, client.user.avatarURL) // Define o rodapé.
         .setColor(embedColor) // E a cor lateral.
