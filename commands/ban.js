5cdba3cc6249ada76ab4e012fafa188b751c1d71
Foguinel <foguinel@gmail.com>
2020-06-01 21:08:00 +0000

    ✅ - Sem necessidade de atualizações

--- commands/ban.js
+++ commands/ban.js
@@ -8,7 +8,22 @@ module.exports.run = async(client, message, errorEmbed) => { // Chama as outras 
 
 if(message.author.id !== "449940691045318656" || !message.member.hasPermission(['MANAGE_ROLES'])); // Primeiramente ele verifica se você tem as permissões de usar este comando.
 
+    const args = message.content.slice(configBot.prefix.length).trim().split(/ +/g); // Define o que são os argumentos.
+
+    var member = message.mentions.members.first()
+    if(!member && args[0])member = args[0]
+    var msg = message.join(" ")
+    var motivo = msg.substr(msg.indexOf(" ") + 1);
+
     database.ref(`Blacklist`)
+    .once('value').then(async function(snap){ // Definimos que agora vamos pegar o valor do módulo.
+    
+        database.ref(`Blacklist/${member}`)
+        .set({
+            reason: motivo
+        })
+
+    })
 
 }
 
