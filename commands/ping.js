34869aa580f5a1b2fa3506c4f27574ee65d2b6f4
Foguinel <foguinel@gmail.com>
2020-06-01 02:14:59 +0000

    ✅ - Sem necessidade de atualizações

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,17 @@
-const Discord = require("discord.js");
-module.exports.run = async(client, message, embedColor) => {
+const Discord = require("discord.js"); // Chama o módulo do Discord
+module.exports.run = async(client, message, embedColor) => { // Chama as outras dependências
 
-if(message.author.id !== "449940691045318656" || !message.member.hasPermission(['MANAGE_GUILDS']));
-const m = await message.channel.send("Ping?");
-let embed = new Discord.MessageEmbed()
-.setAuthor("Conexão 📶", message.author.avatarURL)
-.addField("A latência é de", `${m.createdTimestamp - message.createdTimestamp}ms`)
-.setTimestamp()
-.setFooter(`${client.user.username}`, client.user.avatarURL)
-.setColor(embedColor)
+if(message.author.id !== "449940691045318656" || !message.member.hasPermission(['MANAGE_GUILDS'])); // Primeiramente ele verifica se você tem as permissões de usar este comando.
+const m = await message.channel.send("Ping?"); // Ele envia uma mensagem como referências
 
-m.edit({embed});
+let embed = new Discord.MessageEmbed() // Cria um Embed
+.setAuthor("Conexão 📶", message.author.avatarURL) // Define o título
+.addField("A latência é de", `${m.createdTimestamp - message.createdTimestamp}ms`) // Calcula o tempo necessário para editar a mensagem
+.setTimestamp() // Define o horário da mensagem
+.setFooter(`${client.user.username}`, client.user.avatarURL) // Define o rodapé
+.setColor(embedColor) // E a cor lateral
+
+m.edit({embed}); // Edita a mensagem de referência
 }
 
 module.exports.help = {
