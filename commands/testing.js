e6991902e7f3e4243f7fd46f0866436a7db0ba54
Foguinel <foguinel@gmail.com>
2020-06-05 15:38:21 +0000

    ✅ - Sem necessidade de atualizaçõe

--- commands/testing.js
+++ commands/testing.js
@@ -5,10 +5,10 @@ const database = firebase.database();
 const configBot = require("../configBot.json")
 
 module.exports.run = async(client, message, errorEmbed) => { // Chama as outras dependências
-    
+
 if(message.author.id !== "449940691045318656" || !message.member.hasPermission('MANAGE_ROLES')); // Primeiramente ele verifica se você tem as permissões de usar este comando.
 
-    import { funcGetAtk } from '../index'
+    import { getAtk } from '../index.js'
 
     const args = message.content.slice(configBot.prefix.length).trim().split(/ +/g); // Define o que são os argumentos.
     console.log(funcGetAtk)
