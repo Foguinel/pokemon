5451a091e381e68fb579207502c8e34b8699852e
Foguinel <foguinel@gmail.com>
2020-06-05 14:10:06 +0000

    ✅ - Sem necessidade de atualizações

--- commands/testing.js
+++ commands/testing.js
@@ -44,14 +44,14 @@ if(message.author.id !== "449940691045318656" || !message.member.hasPermission('
             
     //     })
 
-        atk1 = keys[Math.floor(Math.random() * keys.indexOf) + 1]
-        atk2 = keys[Math.floor(Math.random() * keys.indexOf) + 1]
-        atk3 = keys[Math.floor(Math.random() * keys.indexOf) + 1]
-        atk4 = keys[Math.floor(Math.random() * keys.indexOf) + 1]
+        atk1 = Math.floor(Math.random() * keys.indexOf) + 1
+        atk2 = Math.floor(Math.random() * keys.indexOf) + 1
+        atk3 = Math.floor(Math.random() * keys.indexOf) + 1
+        atk4 = Math.floor(Math.random() * keys.indexOf) + 1
 
         let embed = new Discord.MessageEmbed() // Cria um Embed
         .setAuthor(`${name}`, message.author.avatarURL) // Define o título
-        .setDescription(`- ${keys}`) // 
+        .setDescription(`- ${keys[0]}`) // 
         .setTimestamp() // Define o horário da mensagem
         .setFooter(`${client.user.username}`, client.user.avatarURL) // Define o rodapé
         .setColor(0xf8d75d) // E a cor lateral
